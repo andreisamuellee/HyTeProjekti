3e6895b9d3ebebf519e4e51613b142b13ec8183b
mikkoaja <64253688+mikkoaja@users.noreply.github.com>
2020-04-29 15:04:23 +0300

    MPAndroidChart

--- app/src/main/java/com/example/hyteprojekti/SecondActivity.java
+++ app/src/main/java/com/example/hyteprojekti/SecondActivity.java
@@ -2,13 +2,26 @@ package com.example.hyteprojekti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
 
     TextView tv;
     String st, date;
+    //LineChart chart = (LineChart) findViewById(R.id.chart);
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +34,25 @@ public class SecondActivity extends AppCompatActivity {
         date = getIntent().getExtras().getString("Date");
 
         tv.setText(st + " added to day "+ date +" total.");
+        //newArvot();
 
     }
+/*
+    public void newArvot(String x, int y){
+        List<Entry> entries = new ArrayList<Entry>();
+        for (int i = 0; i<10; i++) {
+            // turn your data into Entry objects
+            entries.add(new Entry(i, 2*i));
+        }
+        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
+        dataSet.setColor(Color.BLUE);
+        dataSet.setValueTextColor(Color.RED); // styling, ..
+
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.invalidate(); // refresh
+    }
+  */
+
 }
 

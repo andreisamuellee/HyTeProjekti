fc777a0d9c72f261aef159b2bf0fedcdad04f693
Pekko <pekko.lehto@metropolia.fi>
2020-04-29 11:13:52 +0300

    Second activity

--- app/src/main/java/com/example/hyteprojekti/MainActivity.java
+++ app/src/main/java/com/example/hyteprojekti/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.hyteprojekti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.example.hyteprojekti.R;
+import com.example.hyteprojekti.SecondActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    EditText et;
+    String st, Submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        et = findViewById(R.id.edittext);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                st = et.getText().toString();
+                intent.putExtra("Value",st);
+                startActivity(intent);
+                finish();
+
+            }
+        });
     }
 }

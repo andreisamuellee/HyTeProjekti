caa69688422d8e187b60ab5a162fd1bc84ac8453
Pekko <pekko.lehto@metropolia.fi>
2020-04-29 12:42:48 +0300

    Layout muutoksia.

--- app/src/main/java/com/example/hyteprojekti/MainActivity.java
+++ app/src/main/java/com/example/hyteprojekti/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 
 import com.example.hyteprojekti.R;
 import com.example.hyteprojekti.SecondActivity;
@@ -17,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button btn;
     EditText et;
-    String st, dateString, Submit;
+    String st, dateString;
     javaDate dateOlio;
 
     @Override

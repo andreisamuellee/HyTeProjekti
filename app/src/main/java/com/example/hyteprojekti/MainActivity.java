ba20767461facb67a4e56ab375ea685e6d3bb6e4
mikkoaja <64253688+mikkoaja@users.noreply.github.com>
2020-05-04 11:38:19 +0300

    MPAndroidChart

--- app/src/main/java/com/example/hyteprojekti/MainActivity.java
+++ app/src/main/java/com/example/hyteprojekti/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);0
+        setContentView(R.layout.activity_main);
 
         btn = findViewById(R.id.button);
         et = findViewById(R.id.edittext);
--- app/src/main/java/com/example/hyteprojekti/SecondActivity.java
+++ app/src/main/java/com/example/hyteprojekti/SecondActivity.java
@@ -8,21 +8,27 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.DataSet;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity{
 
+    private static final String TAG = "SecondActivity";
+    private LineChart chart;
     TextView tv;
     String st, date;
     ArrayList<String> days;
     ArrayList<Integer> smokesSmoked;
     javaDate dateOlio;
-    LineChart chart = (LineChart) findViewById(R.id.chart);
+
     int lol = 69;
 
 
@@ -32,6 +38,7 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        testi();
         days = new ArrayList<>();
         smokesSmoked = new ArrayList<>();
         dateOlio = new javaDate();
@@ -51,14 +58,14 @@ public class SecondActivity extends AppCompatActivity {
             smokesSmoked.add(prefGet.getInt(days.get(i-1), 0));
         }
 
-        tv.setText(Integer.toString(smokesSmoked.get(0)) + " added to day "+ days.get(0) +" total.");
+        //tv.setText(Integer.toString(smokesSmoked.get(0)) + " added to day "+ days.get(0) +" total.");
 
         tv.setText(st + " added to day "+ date +" total.");
 
 
     }
 
-    public void newArvot(String x, int y){
+   /* public void newArvot(){
         List<Entry> entries = new ArrayList<Entry>();
         for (int i = 0; i<10; i++) {
             // turn your data into Entry objects
@@ -71,6 +78,31 @@ public class SecondActivity extends AppCompatActivity {
         LineData lineData = new LineData(dataSet);
         chart.setData(lineData);
         chart.invalidate(); // refresh
+
+    } */
+
+    public void testi(){
+        chart = (LineChart) findViewById(R.id.chart);
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(false);
+
+        ArrayList<Entry> yValues = new ArrayList<>();
+
+        yValues.add(new Entry(0,10f));
+        yValues.add(new Entry(1,20f));
+        yValues.add(new Entry(2,30f));
+        yValues.add(new Entry(3,40f));
+
+        LineDataSet set1 = new LineDataSet(yValues, "Data Set 1");
+
+        set1.setFillAlpha(110);
+
+        ArrayList<ILineDataSet>dataSets = new ArrayList<>();
+        dataSets.add(set1);
+
+        LineData data = new LineData(dataSets);
+
+        chart.setData(data);
 
     }
 

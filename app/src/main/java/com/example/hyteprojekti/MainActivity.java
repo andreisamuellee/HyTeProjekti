bece2e87cd71f72e1ff4033e62682a30b813ec28
Pekko <pekko.lehto@metropolia.fi>
2020-05-05 12:36:17 +0300

    Layout muutoksia lisää.

--- app/src/main/java/com/example/hyteprojekti/MainActivity.java
+++ app/src/main/java/com/example/hyteprojekti/MainActivity.java
@@ -9,6 +9,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
